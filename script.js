f5c7254384eecca42b7888da26819c1a057ca25d
Ashish Devikar <148896218+ashish-devikar@users.noreply.github.com>
2024-10-03 02:40:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,8 +52,8 @@ voice.addEventListener("click",()=>{
             speak("mujhe developer ashish sir ne developed kiya hai")
         }else if(message.includes("thank you")||message.includes("thank u")||message.includes("thanks")){
             speak("most welcome")
-        }else if(message.includes("aapko kisne developed kiya hai")||message.includes("who made you")||message.includes("who developed you")){
-            speak("mujhe developer ashish sir ne developed kiya hai")
+        }else if(message.includes("who are you")||message.includes("hu r u")){
+            speak("i am robo, created by my spacial person stylestar Ashish sir")
         }
         else if(message.includes("open youtube")){
             speak("please opening youtube ")
